836fab6075d6c45764948e31ca927adfb512d161
Jakob Bossek <info@jakobbossek.de>
2020-06-29 15:23:20 +0930

    work on mutations

--- ex00_playground.R
+++ ex00_playground.R
@@ -1,6 +1,42 @@
+library(tspgen)
+
+devtools::load_all("~/repos/software/r/ttp")
 source("r-src/ttp-algorithms.R")
 
 prob = "9competitionInstances/a280_n279_bounded-strongly-corr_01.ttp"
+
+coll.kp = tspgen::init()
+#coll.kp = tspgen::addMutator(coll.kp, "doUniformItemsMutation", pm = 0.05, L = 10, R = 4000)
+#coll.kp = tspgen::addMutator(coll.kp, "doCorrelatedItemsMutation", pm = 0.05, L = 10, R = 3900)
+coll.kp = tspgen::addMutator(coll.kp, "doExplosionMutation", pm = 0.3)
+
+coll.tsp = tspgen::init()
+coll.tsp = tspgen::addMutator(coll.tsp, "doExplosionMutation")
+coll.tsp = tspgen::addMutator(coll.tsp, "doClusterMutation")
+
+coll.meta = tspgen::init()
+coll.meta = tspgen::addMutator(coll.meta, "doCapacityMutation")
+
+mut.fun = build_mutation(meta = list(p = 1.0, collection = coll.meta), kp = list(p = 1.0, collection = coll.kp), tsp = list(p = 1.0, collection = coll.tsp))
+
+x = TTP::loadProblem(prob)
+x$coordinates = netgen:::rescaleNetworkGlobal2(x$coordinates) # FIXME: export in netgen
+x$items[, 1:2] = netgen:::rescaleNetworkGlobal2(as.matrix(x$items[, 1:2]))
+y = x
+for (i in 1:100) {
+  y = mut.fun(y)
+}
+
+opar = par(mfrow = c(2, 2))
+plot(x$coordinates)
+plot(x$items[, 2:1])
+plot(y$coordinates)
+plot(y$items[, 2:1])
+par(opar)
+stop("DONE")
+
+# y$items = applyMutationFromCollection(y$items, collection)
+# x$items = doUniformItemsMutation(x$items, min = 1e6, max =1e7)
 
 set.seed(1)
 res = run_ttp_algorithm(prob, algorithm = 24,
--- r-src/ttp-algorithms.R
+++ r-src/ttp-algorithms.R
@@ -99,3 +99,76 @@ build_fitness_function = function(algorithm_a, algorithm_b, n_runs, args) {
   }
   return(fun)
 }
+
+doUniformItemsMutation = function(x, pm = 0.1, L = 1000, R = 10000) {
+  n = nrow(x)
+  k = floor(n * pm)
+  idx = sample(seq_len(n), size = k, replace = FALSE)
+  # column 1:profits, column 2: weights
+  x[idx, 1:2] = matrix(sample(L:R, size = 2 * k, replace = TRUE), ncol = 2L)
+  return(x)
+}
+
+doCorrelatedItemsMutation = function(x, pm = 0.1, L = 10, R = 100, negative_correlation = FALSE) {
+  n = nrow(x)
+  k = floor(n * pm)
+  R10 = R / 10 # for weakly correlated weights
+  corr.factor = if (negative_correlation) -1 else 1
+  idx = sample(seq_len(n), size = k, replace = FALSE)
+  weights.new = sample(L:R, size = k, replace = TRUE)
+  profits.new = floor(runif(k, weights.new - R10, weights.new + R10))
+  x[idx, 1:2] = cbind(profits.new, weights.new)
+  return(x)
+}
+
+doCapacityMutation = function(x, dev = 100) {
+  wsum = sum(x$items$weight)
+  wsum2 = floor(wsum / 2)
+  min = wsum2 - dev
+  max = wsum2 + dev
+  x$capacity = sample(min:max, size = 1L)
+  return(x)
+}
+
+applyMutationFromCollection = function(x, collection) {
+  mutators = collection$mutators
+  n.mutators = length(mutators)
+  names = names(mutators)
+  probs = if (is.null(collection$probs)) {
+    rep(1 / n.mutators, n.mutators)
+  }
+  idx = sample(seq_len(n.mutators), size = 1L, prob = probs)
+  mutator.fun = names[idx]
+  mutator.pars = mutators[[mutator.fun]]
+  mutator.pars = BBmisc::insert(list(x), mutator.pars)
+  y = do.call(mutator.fun, mutator.pars)
+  attr(y, "df") = NULL
+  return(y)
+}
+
+build_mutation = function(...) {
+  types = list(...)
+  print(types)
+  force(types)
+
+  fun = function(x, ...) {
+    checkmate::assertClass(x, "ttp_instance")
+    if (!is.null(types$kp)) {
+      if (runif(1L) < types$tsp$p) {
+        x$items[, 1:2] = applyMutationFromCollection(as.matrix(x$items[, 1:2]), types$kp$collection)
+      }
+    }
+    if (!is.null(types$tsp)) {
+      if (runif(1L) < types$tsp$p) {
+        x$coordinates = applyMutationFromCollection(as.matrix(x$coordinates), types$tsp$collection)
+      }
+    }
+    if (!is.null(types$meta)) {
+      if (runif(1L) < types$meta$p) {
+        x = applyMutationFromCollection(x, types$meta$collection)
+      }
+    }
+    return(x)
+  }
+  ecr::makeMutator(mutator = fun, supported = "custom")
+}
